94e70c9089ff9efd65ecedda96f2509d985c00f0
DrakshaPatle <draksha@appsdeployer.com>
2023-03-28 14:26:45 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
+
+//  import './App.css';
+
+// // import {spreadsheetComponent} from 
+// import { SpreadsheetComponent, SheetsDirective, SheetDirective, RangesDirective, RowsDirective, RowDirective, CellDirective, CellsDirective, getFormatFromType } from '@syncfusion/ej2-react-spreadsheet';
+// import  {Sheet} from  './components/sheet'
+// function App() {
+
+//    <button >Toggle Theme</button>
+
+//   return (
+//     <div className="App">
+      
+//      <SpreadsheetComponent  
+//           // maxHeight={500} 
+//           width={1900}
+//          height={1000}
+//      rowCount={940}
+//      marginTop={400} >
+//      </SpreadsheetComponent>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import React, { useState, useEffect } from 'react';
+import { SpreadsheetComponent, SheetsDirective, SheetDirective, RangesDirective, RowsDirective, RowDirective, CellDirective, CellsDirective, getFormatFromType } from '@syncfusion/ej2-react-spreadsheet';
+import { Sheet } from './components/sheet';
 import './App.css';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('ORg4AjUWIQA/Gnt2VFhhQlJBfVtdX2RWfFN0RnNYfVRxdF9CaEwgOX1dQl9gSX1RfkVjWHdcdn1QRWQ=');
+
+
+
+// function App() {
+//   const [themeMode, setThemeMode] = useState('light');
+
+//   const toggleThemeMode = () => {
+//     setThemeMode(themeMode === 'light' ? 'dark' : 'light');
+//   };
+
+//   useEffect(() => {
+//     import(`./App-${themeMode}.css`).then();
+//   }, [themeMode]);
+
+//   useEffect(() => {
+//     document.body.className = `theme-${themeMode}`;
+//   }, [themeMode]);
+
+//   return (
+//     <div className="App">
+//       <button  className ="btn" onClick={toggleThemeMode}>Toggle Theme</button>
+//       <div className="spreadsheet-container">
+//         <SpreadsheetComponent
+//           className={`spreadsheet-${themeMode}`}
+//           width={1900}
+//           height={1100}
+//           rowCount={940}
+//           marginTop={400}>
+//         </SpreadsheetComponent>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
 function App() {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const toggleThemeMode = () => {
+    setThemeMode(themeMode === 'light' ? 'dark' : 'light');
+  };
+
+  const onBeforeRender = (args) => {
+    if (args.target === 'toolbar') {
+      const toggleButton = {
+        type: 'Button',
+        text: `Toggle Theme (${themeMode})`,
+        tooltipText: 'Toggle Theme',
+        cssClass: 'e-icons e-toggle-theme',
+        click: () => toggleThemeMode(),
+      };
+      args.items.push(toggleButton);
+    }
+  };
+
+  useEffect(() => {
+    import(`./App-${themeMode}.css`).then();
+  }, [themeMode]);
+
+  useEffect(() => {
+    document.body.className = `theme-${themeMode}`;
+  }, [themeMode]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="spreadsheet-container">
+        <SpreadsheetComponent
+          className={`spreadsheet-${themeMode}`}
+          width={1900}
+          height={1050}
+          rowCount={940}
+          marginTop={400}
+          beforeRender={onBeforeRender}>
+          <SheetsDirective>
+            {/* <SheetDirective>
+              <RangesDirective>
+                <RowsDirective>
+                  <RowDirective>
+                    <CellsDirective>
+                      <CellDirective value="Hello World" />
+                    </CellsDirective>
+                  </RowDirective>
+                </RowsDirective>
+              </RangesDirective>
+            </SheetDirective> */}
+          </SheetsDirective>
+        </SpreadsheetComponent>
+      </div>
     </div>
   );
 }
