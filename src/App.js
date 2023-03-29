4462fd930132d2d880ccd35aa16c0f7ce7a6791b
DrakshaPatle <draksha@appsdeployer.com>
2023-03-29 09:53:00 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,34 +1,6 @@
 
-//  import './App.css';
 
-// // import {spreadsheetComponent} from 
-// import { SpreadsheetComponent, SheetsDirective, SheetDirective, RangesDirective, RowsDirective, RowDirective, CellDirective, CellsDirective, getFormatFromType } from '@syncfusion/ej2-react-spreadsheet';
-// import  {Sheet} from  './components/sheet'
-// function App() {
-
-//    <button >Toggle Theme</button>
-
-//   return (
-//     <div className="App">
-      
-//      <SpreadsheetComponent  
-//           // maxHeight={500} 
-//           width={1900}
-//          height={1000}
-//      rowCount={940}
-//      marginTop={400} >
-//      </SpreadsheetComponent>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { SpreadsheetComponent, SheetsDirective, SheetDirective, RangesDirective, RowsDirective, RowDirective, CellDirective, CellsDirective, getFormatFromType } from '@syncfusion/ej2-react-spreadsheet';
 import { Sheet } from './components/sheet';
 import './App.css';
@@ -38,62 +10,18 @@ import { registerLicense } from '@syncfusion/ej2-base';
 registerLicense('ORg4AjUWIQA/Gnt2VFhhQlJBfVtdX2RWfFN0RnNYfVRxdF9CaEwgOX1dQl9gSX1RfkVjWHdcdn1QRWQ=');
 
 
-
-// function App() {
-//   const [themeMode, setThemeMode] = useState('light');
-
-//   const toggleThemeMode = () => {
-//     setThemeMode(themeMode === 'light' ? 'dark' : 'light');
-//   };
-
-//   useEffect(() => {
-//     import(`./App-${themeMode}.css`).then();
-//   }, [themeMode]);
-
-//   useEffect(() => {
-//     document.body.className = `theme-${themeMode}`;
-//   }, [themeMode]);
-
-//   return (
-//     <div className="App">
-//       <button  className ="btn" onClick={toggleThemeMode}>Toggle Theme</button>
-//       <div className="spreadsheet-container">
-//         <SpreadsheetComponent
-//           className={`spreadsheet-${themeMode}`}
-//           width={1900}
-//           height={1100}
-//           rowCount={940}
-//           marginTop={400}>
-//         </SpreadsheetComponent>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
 function App() {
   const [themeMode, setThemeMode] = useState('light');
 
   const toggleThemeMode = () => {
+    
+
+    
     setThemeMode(themeMode === 'light' ? 'dark' : 'light');
   };
 
-  const onBeforeRender = (args) => {
-    if (args.target === 'toolbar') {
-      const toggleButton = {
-        type: 'Button',
-        text: `Toggle Theme (${themeMode})`,
-        tooltipText: 'Toggle Theme',
-        cssClass: 'e-icons e-toggle-theme',
-        click: () => toggleThemeMode(),
-      };
-      args.items.push(toggleButton);
-    }
-  };
+
+
 
   useEffect(() => {
     import(`./App-${themeMode}.css`).then();
@@ -102,18 +30,31 @@ function App() {
   useEffect(() => {
     document.body.className = `theme-${themeMode}`;
   }, [themeMode]);
+  const ssref = useRef(null);
+
+  
 
   return (
     <div className="App">
+
+
       <div className="spreadsheet-container">
         <SpreadsheetComponent
+        
           className={`spreadsheet-${themeMode}`}
-          width={1900}
-          height={1050}
-          rowCount={940}
+          // cssClass='spreadsheet-dark'
+          width={1530}
+          height={830}
+          rowCount={700}
           marginTop={400}
-          beforeRender={onBeforeRender}>
-          <SheetsDirective>
+          
+          // beforeRender={onBeforeRender}
+          >
+
+                <button onClick={(toggleThemeMode)} className="e-btn" >Toggle</button>
+
+          <SheetsDirective
+          > 
             {/* <SheetDirective>
               <RangesDirective>
                 <RowsDirective>
@@ -130,6 +71,6 @@ function App() {
       </div>
     </div>
   );
-}
 
+          }
 export default App;
